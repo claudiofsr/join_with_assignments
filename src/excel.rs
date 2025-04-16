9b9b61239883fc9a4c92b856ce044d86acc5f2bf
claudiofsr <claudiofsr@yahoo.com>
2025-04-15 21:48:35 -0300

    fn remover_colunas_vazias() v5

--- src/excel.rs
+++ src/excel.rs
@@ -121,7 +121,6 @@ pub fn make_worksheet(df: &DataFrame, sheet_name: &str) -> PolarsResult<Workshee
         .set_num_format("dd/mm/yyyy");
 
     dbg!(&df_to_excel);
-    dbg!(&fmt_date);
 
     // Write the dataframe to the worksheet using `PolarsXlsxWriter`.
     PolarsXlsxWriter::new()
